dfe15aad4ed04ec73b1babc68e27dbc9349d89cf
CreativeRouine <vialiysyvashchenko@gmail.com>
2024-04-13 16:31:53 -0500

    timeout added

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -14,7 +14,7 @@ export const connectToDatabase = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
       dbName: "WSE",
-      // serverSelectionTimeoutMS: 30000, 
+      serverSelectionTimeoutMS: 40000, 
       // socketTimeoutMS: 45000, 
     });
     isConnected = true;
