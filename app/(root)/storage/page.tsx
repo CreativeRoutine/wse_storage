129a85493f0d6491f3102464431030d196c5c182
CreativeRouine <vialiysyvashchenko@gmail.com>
2024-04-13 19:53:57 -0500

    chech added

--- app/(root)/storage/page.tsx
+++ app/(root)/storage/page.tsx
@@ -2,8 +2,6 @@
 import React from "react";
 import Title from "@/components/shared/Title";
 import { getPallets } from "@/lib/actions/pallet.action";
-
-
 import DisplayPallets from "@/components/shared/DisplayPallets";
 
 const Storage = async () => {
@@ -11,20 +9,28 @@ const Storage = async () => {
   const resultPallets = await getPallets({})
   const pallets = JSON.parse(JSON.stringify(resultPallets.pallets))
   
-  return (
-    <>
-      <Title text="Storage" />
-      
-      <div className="py-4 px-8 mb-2 bg-dark-600 text-white rounded-xl flex items-center justify-between border border-dark-350 shadow-lg">
-        <div className="w-full text-lg">
-          Pallets without printers.
+  if(pallets){
+    return (
+      <>
+        <Title text="Storage" />
+        
+        <div className="py-4 px-8 mb-2 bg-dark-600 text-white rounded-xl flex items-center justify-between border border-dark-350 shadow-lg">
+          <div className="w-full text-lg">
+            Pallets without printers.
+          </div>
         </div>
-      </div>
-
-      <DisplayPallets pallets={pallets}/>
-      
-    </>
-  );
+  
+        <DisplayPallets pallets={pallets}/>
+        
+      </>
+    );
+  } else{
+    return( 
+      <>
+      <div>NOTHING TO DISPLAY</div>
+      </>
+     )
+  }
 };
 
 export default Storage;
