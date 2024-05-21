f008c182859d66469785ff39bc2d147ebceb114c
CreativeRouine <vialiysyvashchenko@gmail.com>
2024-05-21 12:36:04 -0500

    added ponumber to printers

--- lib/validations.ts
+++ lib/validations.ts
@@ -45,7 +45,7 @@ export const deletePalletSchema = z.object({
 // ===================  Printer  ===================
 
 export const addPrinterSchema = z.object({
-  ponumber: z.string().min(3).max(12),
+  ponumber: z.string().min(0).max(12),
   sn: z.string().min(5).max(12), 
   productNumber: z.string().min(3).max(12),
   barcode: z.string().min(3).max(12),
