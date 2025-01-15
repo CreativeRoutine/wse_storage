c4728d0df806117e758ad89b774fe21d0e86f439
Vitaliy Syvashchenko <vitaliysyvashchenko@WIN-TG2FTP28UFP.whitespider.local>
2025-01-15 16:24:55 -0600

    barcodes for parts

--- database/barcodes.model.ts
+++ database/barcodes.model.ts
@@ -2,18 +2,18 @@ import { Schema, model, models, Document } from "mongoose";
 import { string } from "zod";
 
 export interface IBarcodes extends Document {
-  parts: number;
-  printers: number;
-  pallets: number;
-  storage: string;
+  parts?: number;
+  printers?: number;
+  pallets?: number;
+  storage?: string;
   
 }
 
 const BarcodesSchema = new Schema({
-    parts: { type: Number, required: true },
-    printers: { type: Number, required: true },
-    pallets: { type: Number, required: true },
-    storage: { type: String, required: true },
+    parts: { type: Number, required: false },
+    printers: { type: Number, required: false },
+    pallets: { type: Number, required: false },
+    storage: { type: String, required: false },
 });
 
 const Barcodes = models.Barcodes || model("Barcodes", BarcodesSchema);
